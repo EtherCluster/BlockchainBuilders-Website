bd1742b416d8fc6d89c03479ddd791a8dd9fc51f
EtherCluster <ethercluster@protonmail.com>
2021-11-03 16:31:20 +0200

    under dev

---
+++ blockchainbuilderswebsite/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+        This website is under development :) stay tuned
+        </p>
+        <a
+          className="App-link"
+          href="https://discord.gg/yJaNgsbpDr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Discord
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
